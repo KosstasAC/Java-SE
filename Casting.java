ff33e383da5374a47b9aa47b803e0502d652227e
Kosstas Acuna <aaa.kosstas@gmail.com>
2024-03-14 16:03:21 -0700

    first commit - commiting all files to the repository

---
+++ Casting.java
@@ -0,0 +1,11 @@
+public class Casting {
+    public static void main (String[] args){
+        // Casting is helpful for the need to change of one data type to another.
+
+        int a = 30;
+        int b = 12;
+       
+        System.out.println((double) a/b);
+        System.out.println((long) a/b);
+    }
+}
---
+++ IncrementDecrement.java
@@ -0,0 +1,15 @@
+public class IncrementDecrement {
+    public static void main (String [] args){
+
+        int lives = 5;
+        lives = lives - 1;;
+        System.out.println(" "); //
+
+        lives--; //Decremento
+        System.out.println(lives);
+
+        lives++; //Incremento
+        System.out.println(lives);
+
+    }
+}
